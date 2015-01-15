693bd03f2d1eba1d3504443179918528c2f7e347
姜天文 <1346729091@qq.com>
2015-01-15 18:04:19 +0800

    20150115

---
+++ controlOrder.class.php
@@ -0,0 +1,21 @@
+<?php
+	require_once("./db.class.php");
+	require_once("./order.class.php");
+	
+	/**
+	 *类名：Order
+	 *说明：义诊预约订单控制类
+	 */
+	class ControlOrder{
+		public static function insert_order($order){
+			if(is_a($order, 'Order')){
+				echo "is a Order";
+				$db = new DB();
+	            $resultid = $db->insertData("orders",array(),array('', $order->name, $order->phone, $order->problem));
+	            return true;
+			}else{
+				return false;
+			}
+		}
+	}
+?>
--- index.php
+++ index.php
@@ -4,6 +4,9 @@
   */
 
 //define your token
+require_once("order.class.php");
+require_once("controlOrder.class.php");
+
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
 if (isset($_GET['echostr'])) {
@@ -11,7 +14,6 @@ if (isset($_GET['echostr'])) {
 }else{
     $wechatObj->checkMsg();
 }
-
 
 class wechatCallbackapiTest
 {
@@ -80,7 +82,16 @@ class wechatCallbackapiTest
                 if(!empty( $keyword ))
                 {
                     $msgType = "text";
-                    $content = "预约成功";
+                    $keyword =explode('-',$keyword);
+                    //$content = "预约成功\n"."电话：".$keyword[1]."\n姓名：".$keyword[2]."\n问题：".$keyword[3];
+                    $content = "预约失败，请输入：预约-电话-姓名-问题";
+                    
+                    if(sizeof($keyword)==4)
+                    {
+                        $userOrder = new Order($keyword[2], $keyword[1], $keyword[3]);
+                        ControlOrder::insert_order($userOrder);
+                        $content = "预约成功\n"."姓名：".$userOrder->name."\n电话：".$userOrder->phone."\n问题：".$userOrder->problem;
+                    }
                     
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $content);
                     echo $resultStr;
@@ -153,27 +164,27 @@ class wechatCallbackapiTest
 
                     $title1 = "笔记本选购指南 （作者：电脑110技术顾问 孤独凤凰战士）";
                     $description1 = "哈工大电脑110俱乐部";
-                    $picUrl1 = "http://1.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl1 = "http://1.diannao110weixin.sinaapp.com/image/1.jpg";
                     $url1 = "http://user.qzone.qq.com/1507733185/blog/1420944883";
                     
                     $title2 = "【小幺经验谈】电脑开不开机？且开且珍惜";
                     $description2 = "哈工大电脑110俱乐部";
-                    $picUrl2 = "http://2.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl2 = "http://1.diannao110weixin.sinaapp.com/image/110.jpg";
                     $url2 = "http://user.qzone.qq.com/1507733185/blog/1420457404";
 
                     $title3 = "“微会”免费电话原理小探究";
                     $description3 = "哈工大电脑110俱乐部";
-                    $picUrl3 = "http://2.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl3 = "http://1.diannao110weixin.sinaapp.com/image/110.jpg";
                     $url3 = "http://user.qzone.qq.com/1507733185/blog/1420285932";
 
                     $title4 = "Win7系统的C盘扩容（正确操作不会损坏磁盘资料）";
                     $description4 = "哈工大电脑110俱乐部";
-                    $picUrl4 = "http://2.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl4 = "http://1.diannao110weixin.sinaapp.com/image/110.jpg";
                     $url4 = "http://user.qzone.qq.com/1507733185/blog/1420091111";
 
                     $title5 = "拆机清灰步骤图文详解 ps:适用于大多数笔记本电脑";
                     $description5 = "哈工大电脑110俱乐部";
-                    $picUrl5 = "http://2.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl5 = "http://1.diannao110weixin.sinaapp.com/image/110.jpg";
                     $url5 = "http://user.qzone.qq.com/1507733185/blog/1419992711";
                     
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, 
@@ -239,17 +250,17 @@ class wechatCallbackapiTest
                     $msgType = "news";
                     $title = "哈工大电脑110俱乐部";
                     $description = "哈工大电脑110俱乐部";
-                    $picUrl = "http://1.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl = "http://1.diannao110weixin.sinaapp.com/image/1.jpg";
                     $url = "http://mp.weixin.qq.com/s?__biz=MzA4MDMzMjkwNg==&mid=203657939&idx=1&sn=29e673ce63cd5f5b257487bd58b83cfc#rd";
 
                     $title1 = "微信平台【义诊预约】功能说明";
                     $description1 = "哈工大电脑110俱乐部";
-                    $picUrl1 = "http://2.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl1 = "http://1.diannao110weixin.sinaapp.com/image/1.jpg";
                     $url1 = "http://mp.weixin.qq.com/s?__biz=MzA4MDMzMjkwNg==&mid=203657939&idx=2&sn=350a553cfbf13de0763b56ed4cb9bc47#rd";
                     
                     $title2 = "微信平台【电脑110小幺】功能说明";
                     $description2 = "哈工大电脑110俱乐部";
-                    $picUrl2 = "http://2.tianwenweixintest.sinaapp.com/image/1.jpg";
+                    $picUrl2 = "http://1.diannao110weixin.sinaapp.com/image/1.jpg";
                     $url2 = "http://mp.weixin.qq.com/s?__biz=MzA4MDMzMjkwNg==&mid=203659892&idx=1&sn=12e47a9f76965fd473add8d2b5528ecd#rd";
                     
                     $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, 
